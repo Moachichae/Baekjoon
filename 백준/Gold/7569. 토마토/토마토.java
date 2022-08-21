75d6404035b82e519d83af5c66bca8c9af0cfd88
seungho chae <60034386+Moachichae@users.noreply.github.com>
2022-08-21 22:58:10 +0900

    [Gold V] Title: 토마토, Time: 724 ms, Memory: 120016 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -7,13 +7,10 @@ public class Main {
         int n;
         int h;
 
-        int day;
-
-        public Node(int m, int n, int h,int day) {
+        public Node(int m, int n, int h) {
             this.m = m;
             this.n = n;
             this.h = h;
-            this.day = day;
         }
     }
     static boolean[][][] isVisit = new boolean[100][100][100];
@@ -25,7 +22,8 @@ public class Main {
     static int[] dn = {0, 1, 0, -1, 0, 0};
     static int[] dh = {0, 0, 0, 0, 1, -1};
     static Queue<Node> que = new LinkedList<>();
-    static int answer = 0;
+    static int unripe = 0; //덜 익은 토마토
+    static int day = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -41,9 +39,11 @@ public class Main {
                 st = new StringTokenizer(br.readLine());
                 for (int m = 0; m < M; m++) {
                     graph[m][n][h] = Integer.parseInt(st.nextToken());
-                    if (!isVisit[m][n][h] && graph[m][n][h] == 1){
+                    if (graph[m][n][h] == 1){
                         isVisit[m][n][h] = true;
-                        que.offer(new Node(m, n, h,0));
+                        que.offer(new Node(m, n, h));
+                    }else if(graph[m][n][h] ==  0){
+                        unripe ++;
                     }
                 }
             }
@@ -51,45 +51,40 @@ public class Main {
 
         bfs();
 
-        for (int h = 0; h < H; h++) {
-            for (int n = 0; n < N; n++) {
-                for (int m = 0; m < M; m++) {
-                    if (graph[m][n][h] == 0){
-                        answer = -1;
-                    }
-                }
-            }
-        }
 
-        System.out.println(answer);
+        System.out.println(unripe > 0 ? -1 : day);
 
     }
 
 
     static void bfs(){
 
-        while (!que.isEmpty()){
+        while (unripe > 0 && !que.isEmpty()){
 
-            Node node = que.poll();
-            graph[node.m][node.n][node.h] = 1;
-            answer = Math.max(answer, node.day);
-            for (int i = 0; i < 6; i++) {
-                int nm = node.m + dm[i];
-                int nn = node.n + dn[i];
-                int nh = node.h + dh[i];
+            int count = que.size();
+            for (int i = 0; i < count; i++) {
+                Node node = que.poll();
 
-                if (nm < 0 || nm >= M || nn < 0 || nn >= N || nh < 0 || nh >= H) continue;
+                for (int k = 0; k < 6; k++) {
+                    int nm = node.m + dm[k];
+                    int nn = node.n + dn[k];
+                    int nh = node.h + dh[k];
 
-                if (!isVisit[nm][nn][nh] && graph[nm][nn][nh] == 0){
-                    isVisit[nm][nn][nh] = true;
-                    que.offer(new Node(nm, nn, nh, node.day + 1));
+                    if (nm < 0 || nm >= M || nn < 0 || nn >= N || nh < 0 || nh >= H) continue;
+
+                    if (!isVisit[nm][nn][nh] && graph[nm][nn][nh] == 0){
+                        isVisit[nm][nn][nh] = true;
+                        graph[node.m][node.n][node.h] = 1;
+                        unripe --;
+                        que.offer(new Node(nm, nn, nh));
+                    }
+
                 }
-
             }
+
+            day++;
         }
 
     }
-
-
 
 }
